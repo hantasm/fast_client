f1f79aff644a3675860d06f8c9abfde855c9fc4a
Hermann He <myhechh@gmail.com>
2015-09-18 03:48:09 -0400

    first commit, basic functions, no game logic

---
+++ myscripts.js
@@ -0,0 +1,37 @@
+
+var request = "request"
+var response = "response"
+
+function displayRequest() {
+    document.getElementById("reqText").innerHTML = request;
+}
+
+function displayResponse() {
+    document.getElementById("resText").innerHTML = response;
+}
+
+function clickStart() {
+    displayRequest()
+}
+
+function clickGet() {
+
+}
+
+function clickSubmit() {
+
+}
+
+function clickSend() {
+
+    displayResponse()
+}
+
+function clickNext() {
+
+}
+
+function clickGuess() {
+
+}
+
